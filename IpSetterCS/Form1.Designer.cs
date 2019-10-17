009eefc9caa466b47307fda5dc82a4cf3a1c59a3
Kenny Quast <kenny@quast.ca>
2019-10-16 23:54:48 -0400

    coded exit button, and about button. Also changed startup to center screen for now

--- IpSetterCS/Form1.Designer.cs
+++ IpSetterCS/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSetIP = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnReturnIP = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // btnSetIP
+            // 
+            this.btnSetIP.Location = new System.Drawing.Point(12, 32);
+            this.btnSetIP.Name = "btnSetIP";
+            this.btnSetIP.Size = new System.Drawing.Size(129, 51);
+            this.btnSetIP.TabIndex = 1;
+            this.btnSetIP.Text = "Set IP";
+            this.btnSetIP.UseVisualStyleBackColor = true;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(12, 89);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(129, 51);
+            this.btnAbout.TabIndex = 2;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.BtnAbout_Click);
+            // 
+            // btnReturnIP
+            // 
+            this.btnReturnIP.Location = new System.Drawing.Point(147, 32);
+            this.btnReturnIP.Name = "btnReturnIP";
+            this.btnReturnIP.Size = new System.Drawing.Size(129, 51);
+            this.btnReturnIP.TabIndex = 3;
+            this.btnReturnIP.Text = "Return to DHCP";
+            this.btnReturnIP.UseVisualStyleBackColor = true;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(147, 89);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(129, 51);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.BtnQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(298, 145);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnReturnIP);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnSetIP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSetIP;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnReturnIP;
+        private System.Windows.Forms.Button btnQuit;
     }
 }
 
--- IpSetterCS/Form1.cs
+++ IpSetterCS/Form1.cs
@@ -16,5 +16,19 @@ namespace IpSetterCS
         {
             InitializeComponent();
         }
+
+        private void BtnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a down and dirty way for me to change my Ip address from DHCP to STATIC and back, while working on PLC's, Cameras and other equiptment at work", "About IP Setter v0.0.0.1");
+        }
     }
+
+
+
 }
+
