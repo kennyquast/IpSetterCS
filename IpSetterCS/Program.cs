3644b1c730ab73f51bba5177eb68d2d2ad709e25
Kenny Quast <kenny@quast.ca>
2019-10-19 00:10:09 -0400

    added a check to dis-allow multiple instances of software running

--- IpSetterCS/Program.cs
+++ IpSetterCS/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Drawing;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace IpSetterCS
 {
@@ -15,13 +12,40 @@ namespace IpSetterCS
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run (new Form1());
-          
+            try
+            {
+                if (IsApplicationAlreadyRunning() == true)
+                {
+                    MessageBox.Show("The application is already running");
+                }
+                else
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("error " + err.ToString());
+            }
+        }
+        static bool IsApplicationAlreadyRunning()
+        {
+            string proc = Process.GetCurrentProcess().ProcessName;
+            Process[] processes = Process.GetProcessesByName(proc);
+            if (processes.Length > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
 
         }
-        
+       
     }
 }
 
